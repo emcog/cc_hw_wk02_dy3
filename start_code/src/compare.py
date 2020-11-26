54f714785fccd62fd65d83d1f8bd412b69e89ae2
emCog <robin.leonard@gmail.com>
2020-11-26 08:53:40 +0000

    test working & passed

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,2 +1,7 @@
-def compare():
-    pass
+def compare(num_1, num_2):
+    if num_1 > num_2:
+        return f'{ num_1 } is greater than { num_2 }'
+    elif num_1 < num_2:
+        return f'{ num_1 } is less than { num_2 }'
+    elif num_1 == num_2:
+        return f'{ num_1 } equals { num_2 }'
--- start_code/tests/compare_test.py
+++ start_code/tests/compare_test.py
@@ -7,11 +7,4 @@ class TestCompare(unittest.TestCase):
         self.assertEqual("3 is greater than 1", compare(3, 1))
 
     def test_compare_equals(self):
-        self.assertEqual(" 5 == 5", compare(5, 5))
-
-
-
-#     Write a test that will make you handle the situation when both numbers are the same
-# Run the test and see check it fails (and that you've written it correctly!)
-# Write the code to pass the test
-# Run the code and check the tests pass (or fix any errors and run the tests again)
+        self.assertEqual("5 equals 5", compare(5, 5))  
